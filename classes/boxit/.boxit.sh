5979909e085f78d40167428d4115dc21f4cebe5c
Syafiqq <syafiq.rezpector@gmail.com>
2016-11-22 15:14:57 +0700

    Change generator name

--- classes/boxit/.boxit.sh
+++ classes/boxit/.boxit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-_project_dir="/home/syafiq/Documents/JetBrains/CLion/hackerrank.cpp/"
-_this_name="generator.sh"
+_project_dir="/home/syafiq/Documents/JetBrains/Clion/hackerrank.cpp/"
+_this_name=".boxit.sh"
 _path=$(readlink -f generator.sh | replace ${_project_dir} "" | replace ${_this_name} "")
 cd ../../
 find ${_path} -type f \( -iname "*.hpp" -or -iname "*.cpp" -or -iname "*.cxx" -or -iname "*.c" -or -iname "*.h" \) -printf '%p\n' | sort | cat > ${_path}list.properties
